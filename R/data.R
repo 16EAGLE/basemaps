e4a17d3948ea1cd4cdc4854921fce49bf1f5a680
16EAGLE <jxsw@web.de>
2021-05-14 10:50:15 +0200

    updated example data

--- R/data.R
+++ R/data.R
@@ -1,9 +1,16 @@
-#' Example extemt
+#' Example extent
 #'
-#' The example dataset contains the \code{sf} objects \code{ext} and \code{ext_eur} that can be used to call \code{\link{basemap}} and the associated functions.
+#' The example datasets contain the \code{sf} objects \code{ext} and \code{ext_eur} that can be used to call \code{\link{basemap}} and the associated functions.
 #' 
 #' @format \code{sf} object
-#' @usage data(ext)
+#' 
 #' @docType data
+#' @usage data(ext)
+#' 
+#' @name data
 #' 
 "ext"
+
+#' @rdname data
+#' @usage data(ext_eur)
+"ext_eur"
